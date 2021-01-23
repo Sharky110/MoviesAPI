10328a4b311be01b45463afd03c26b85cd1e53fe
Sergey <ilyasovsa93@gmail.com>
2021-01-23 19:53:44 +0300

    Rename project

--- MoviesAPI/Startup.cs
+++ MoviesAPI/Startup.cs
@@ -2,13 +2,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace MoviesAPI
+namespace Movies.Api
 {
     public class Startup
     {
@@ -16,6 +17,12 @@ namespace MoviesAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.
+                AddDbContext<Integration1CContext>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("Integration1CContext"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
